5467cee635063127852842bf77b8c8b39c4ad790
develaper <ibrahim.lachguer@gmail.com>
2023-06-11 18:58:16 +0200

    Added rover move_forward and spin_left, spin_right

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,2 +1,36 @@
 class MarsRover
+  attr_accessor :x, :y, :direction
+  DIRECTIONS = ['N', 'E', 'S', 'W']
+  X_DIRECTIONS = {'E':1, 'W': -1}
+  Y_DIRECTIONS = {'N':1, 'S': -1}
+
+  def initialize(x, y, direction)
+    @x = x
+    @y = y
+    @direction = direction
+  end
+  def move_forward
+    if moving_through_y?
+      @y = @y + Y_DIRECTIONS[@direction.to_sym]
+    elsif moving_through_x?
+      @x = @x + X_DIRECTIONS[@direction.to_sym]
+    end
+  end
+  def spin_left
+    new_index = (DIRECTIONS.index(@direction) - 1)%4 
+    @direction = DIRECTIONS[new_index]
+  end
+  def spin_right
+    new_index = (DIRECTIONS.index(@direction) + 1)%4 
+    @direction = DIRECTIONS[new_index]
+  end
+  
+
+  def moving_through_x?
+    X_DIRECTIONS.keys.include?(@direction.to_sym)
+  end
+
+  def moving_through_y?
+    Y_DIRECTIONS.keys.include?(@direction.to_sym)
+  end
 end
